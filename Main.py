61ef39fdf084d89df0801cb4e649d08fcdd1cfe4
Rory <mr.retroman@hotmail.co.uk>
2016-12-21 18:43:55 +0000

    Removed redundant abstraction of the api variable in the the class Bot(). api is now just a global variable instead. Also improved efficency of re_tweet and tweet by moveing the file read in code outside there job loops. All files are now read in at start.

--- Main.py
+++ Main.py
@@ -10,50 +10,52 @@ from queue import Queue
 import random
 import time
 
-
-# This class defines a bot and sets up the keys for OAuth with the twitter api.
-class Bot(object):
-    def __init__(self):
-        # Reading in keys from text file.
-        f = open("Keys.txt", "r")
-        temp = f.readlines()
-        f.close()
-        keys = []
-        # Strips newline characters from the read in keys.
-        for key in temp:
-            keys.append(key.rstrip())
-        c_key = keys[0]
-        c_secret = keys[1]
-        t_key = keys[2]
-        t_secret = keys[3]
-        # Sets up the api for use.
-        self.api = twitter.Api(consumer_key=c_key,
-                               consumer_secret=c_secret,
-                               access_token_key=t_key,
-                               access_token_secret=t_secret)
-
-# This is where you define your bot instances and set up the queue and lock for the threads. I called my bot Gleeson.
-Gleeson = Bot()
+# Reading in keys from text file.
+f = open("Keys.txt", "r")
+temp = f.readlines()
+f.close()
+keys = []
+# Strips newline characters from the read in keys.
+for key in temp:
+    keys.append(key.rstrip())
+c_key = keys[0]
+c_secret = keys[1]
+t_key = keys[2]
+t_secret = keys[3]
+# Sets up the api for use.
+api = twitter.Api(consumer_key=c_key,
+                  consumer_secret=c_secret,
+                  access_token_key=t_key,
+                  access_token_secret=t_secret)
+# Reading in hashtags from text file.
+f = open("Hashtags.txt", "r")
+temp = f.readlines()
+f.close()
+hashtags = []
+# Strips newline characters from the read in hashtags.
+for line in temp:
+    hashtags.append(line.rstrip())
+# Reading in tweets from text file.
+f = open("Tweets.txt", "r")
+temp = f.readlines()
+f.close()
+tweets = []
+# Strips newline characters from the read in tweets.
+for line in temp:
+    tweets.append(line.rstrip())
+# Creates the job queue.
 queue = Queue()
 
 
 # This job is for sending out random pre-written tweets periodically.
-def tweet(worker):
-    # Reading in tweets from text file.
-    f = open("Tweets.txt", "r")
-    temp = f.readlines()
-    f.close()
-    tweets = []
-    # Strips newline characters from the read in tweets.
-    for line in temp:
-        tweets.append(line.rstrip())
+def tweet(worker_thread):
     # This number is used to pick a random tweet.
-    number = random.randrange(0, len(temp))
+    number = random.randrange(0, len(tweets))
     # Tries to post tweet but built in exception in case the tweet fails.
     post = 0
     while post == 0:
         try:
-            Gleeson.api.PostUpdates(tweets[number])
+            api.PostUpdates(tweets[number])
             print("Tweeted.")
             post = 1
         except twitter.error.TwitterError:
@@ -61,27 +63,17 @@ def tweet(worker):
 
 
 # This job is for re-tweeting tweets periodically.
-def re_tweet(worker):
+def re_tweet(worker_thread):
     # Tries to make a re-tweet but has a built in exception in case it fails.
-    # Reading in tweets from text file.
-    f = open("Hashtags.txt", "r")
-    temp = f.readlines()
-    f.close()
-    hashtags = []
-    # Strips newline characters from the read in tweets.
-    for line in temp:
-        hashtags.append(line.rstrip())
     post = 0
     while post == 0:
         try:
             # This number is used to pick a random hashtag for the search.
-            number = random.randrange(0, len(temp))
+            number = random.randrange(0, len(hashtags))
             hashtag = hashtags[number]
-            result = Gleeson.api.GetSearch(
-                raw_query="q=%23"+hashtag+" lang%3Aen&count=1")
-            for data in result:
-                retweet_id = data.id_str
-            Gleeson.api.PostRetweet(retweet_id)
+            # Searched for a tweet using a random hashtag and posts the first result.
+            api.PostRetweet(api.GetSearch(
+                raw_query="q=%23"+hashtag+" lang%3Aen&count=1")[0].id_str)
             print("Re-Tweeted.")
             post = 1
         except twitter.error.TwitterError:
@@ -92,14 +84,14 @@ def re_tweet(worker):
 def threader():
     # Main programme loop.
     while True:
-        worker = queue.get()
+        worker_thread = queue.get()
         number = random.randint(0, 1)
         # The worker is assigned a random job based off the randomly generated number variable.
         if number == 0:
-            tweet(worker)
+            tweet(worker_thread)
             queue.task_done()
         elif number == 1:
-            re_tweet(worker)
+            re_tweet(worker_thread)
             queue.task_done()
         else:
             print("Threader number error.")
